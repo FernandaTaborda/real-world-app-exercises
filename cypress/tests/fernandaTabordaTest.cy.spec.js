dc7c51b3ee36ea5997123429b97766f33c96a0ef
Fernanda Taborda <carneirotaborda@hotmail.com>
2024-09-09 15:09:48 +0100

    Money transfer automation added

--- cypress/tests/fernandaTabordaTest.cy.spec.js
+++ cypress/tests/fernandaTabordaTest.cy.spec.js
@@ -1,7 +1,11 @@
 import loginPage from '../pages/loginPage.js'
+import registerPage from '../pages/registerPage.js'
 import userData from '../fixtures/userData.json'
+import moneyTransfer from '../pages/moneyTransferPage.js'
 
 const login = new loginPage
+const register = new registerPage
+const transfer = new moneyTransfer
 
 describe('Login', () => {
   it('Valid login', () => {
@@ -15,3 +19,36 @@ describe('Login', () => {
     login.ckeckLoginInvalid()
   })
 })
+
+describe.only('Register', () => {
+  it('Fill personal details', () => {
+    register.acessSignUp()
+    register.fillFieldsSignUp(userData.personalDetails)
+  })
+})
+
+describe('Money transfer', () => {
+  it('Transfer with any value for any person', () =>{
+      login.acessLoginPage()
+      login.loginWithAnyUser(userData.validLogin, userData.validLogin)
+      transfer.acessTransferPage()
+      transfer.transferMoney(userData.transferDetails)
+      transfer.checkValidTrasnsferMoney()
+  })
+  it('Invalid transfer', () =>{
+    login.acessLoginPage()
+    login.loginWithAnyUser(userData.validLogin, userData.validLogin)
+    transfer.acessTransferPage()
+    transfer.transferMoney(userData.invalidTransferDetails)
+    transfer.checkInvalidTrasnsferMoney()
+  })
+})
+
+describe('Transaction history', () => {
+  it('View transaction history', () =>{
+    login.acessLoginPage()
+    login.loginWithAnyUser(userData.validLogin, userData.validLogin)
+    transfer.viewTransactionHistory()
+  })
+})
+
